26a9bce09a8294fa7a0d5860bee6f215ebaf9114
Martin N <martinnabhan@me.com>
2015-08-27 20:30:41 +0000

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-  format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+                         format: { with: VALID_EMAIL_REGEX },
+                         uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, allow_blank: true
   
   # Returns hash digest of given string
   def User.digest(string)
